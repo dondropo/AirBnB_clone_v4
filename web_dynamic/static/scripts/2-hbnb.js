48eb911d6618d080404d99e095661c761a185ca2
Ricardo Hincapie <hincapiet.ricardo@gmail.com>
2020-09-28 23:29:40 +0000

    Add task 3

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,34 @@
+const myListId = [];
+const myListName = [];
+$(document).ready(function () {
+  $('input[type=checkbox]').change(function () {
+    if ($(this).prop('checked') === true) {
+      myListId.push($(this).attr('data-id'));
+      console.log(myListId);
+      myListName.push($(this).attr('data-name'));
+      console.log(myListName);
+    } else {
+      const index = $.inArray($(this).attr('data-id'), myListId);
+      if (index !== -1) {
+        myListId.splice(index, 1);
+        const name = $.inArray($(this).attr('data-name'), myListName);
+        if (name !== -1) {
+          myListName.splice(name, 1);
+        }
+      }
+    }
+    if (myListId.length === 0) {
+      $('.amenities h4').html('&nbsp');
+    } else {
+      $('.amenities h4').text(myListName.join(', '));
+      $('.amenities h4').append('...');
+    }
+    })
+    $.getJSON('http://0.0.0.0:5001/api/v1/status/', function (data) {
+      if (data.status === 'OK') {
+          $('#api_status').addClass('available')
+        } else {
+          $('#api_status').removeClass('available')
+        }
+  });
+});
